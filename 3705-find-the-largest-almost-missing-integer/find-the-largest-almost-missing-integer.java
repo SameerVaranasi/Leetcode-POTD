81fc3de3c5792632f8cbe8576314151f3ec629cc
Sameer Varanasi <sameerr.varanasi@gmail.com>
2026-08-18 13:21:32 +0530

    Time: 6 ms (37.19%) | Memory: 46.5 MB (24.21%) - LeetSync

--- 3705-find-the-largest-almost-missing-integer/find-the-largest-almost-missing-integer.java
+++ 3705-find-the-largest-almost-missing-integer/find-the-largest-almost-missing-integer.java
@@ -1,24 +1,49 @@
 class Solution {
     public int largestInteger(int[] nums, int k) {
-        HashMap<Integer,Integer> map = new HashMap<>();
-        for (int i =0;i<=nums.length-k;i++){ //checking the loop
-            HashSet<Integer> set = new HashSet<>();
-            for(int j = i; j<i+k;j++){
-                set.add(nums[j]);
-                //close the loop
-            }
-            // another for loop
-            for(int num:set){
-                map.put(num,map.getOrDefault(num,0)+1); // make note of this line for future ref. from gpt**
-            }
+
+        HashMap<Integer, Integer> window = new HashMap<>();
+        HashMap<Integer, Integer> count = new HashMap<>();
+
+        // First window
+        for (int i = 0; i < k; i++) {
+            window.put(nums[i], window.getOrDefault(nums[i], 0) + 1);
         }
-        int ans =-1;
-        for(Map.Entry<Integer,Integer> entry : map.entrySet()){
-            if(entry.getValue()==1){//fumctiom** from gemini new one
-                ans= Math.max(ans,entry.getKey());
+
+        // Count distinct elements in first window
+        for (int num : window.keySet()) {
+            count.put(num, count.getOrDefault(num, 0) + 1);
+        }
+
+        // Slide the window
+        for (int i = k; i < nums.length; i++) {
+
+            int remove = nums[i - k];
+            int add = nums[i];
+
+            // Remove left element
+            window.put(remove, window.get(remove) - 1);
+
+            if (window.get(remove) == 0) {
+                window.remove(remove);
             }
 
+            // Add right element
+            window.put(add, window.getOrDefault(add, 0) + 1);
+
+            // Current distinct elements
+            for (int num : window.keySet()) {
+                count.put(num, count.getOrDefault(num, 0) + 1);
+            }
         }
+
+        int ans = -1;
+
+        for (Map.Entry<Integer, Integer> entry : count.entrySet()) {
+            if (entry.getValue() == 1) {
+                ans = Math.max(ans, entry.getKey());
+            }
+        }
+
         return ans;
     }
 }
