06923aaa984f00a158a38e25b0096aa9511b8aad
Sameer Varanasi <sameerr.varanasi@gmail.com>
2026-09-06 11:27:04 +0530

    Time: 14 ms (92.72%) | Memory: 54.4 MB (38.68%) - LeetSync

---
+++ 115-distinct-subsequences/distinct-subsequences.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int numDistinct(String s, String t) {
+        int m = s.length();
+        int n = t.length();
+        if(m<n){
+            return 0;
+        }
+        int [][] dp = new int [m+1][n+1];
+        for(int i = 0 ; i <= m ; i++){
+            dp[i][n]=1;
+
+        }
+        for(int i = m-1 ; i>=0 ; i--){
+            char sc = s.charAt(i);
+            for(int j = n-1 ; j>=0 ; j--){
+                char tc = t.charAt(j);
+                if(sc==tc){
+                    dp[i][j]=dp[i+1][j+1]  + dp[i+1][j];
+                }else{
+                    dp[i][j]=dp[i+1][j];
+                }
+            }
+        }
+        return dp[0][0];
+    }
+}
