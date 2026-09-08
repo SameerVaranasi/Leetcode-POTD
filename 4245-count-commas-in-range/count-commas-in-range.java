9a8ffe32cd876cdf0501cd7340fef11a39679cbd
Sameer Varanasi <sameerr.varanasi@gmail.com>
2026-09-09 00:54:38 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (72.85%) - LeetSync

--- 4245-count-commas-in-range/count-commas-in-range.java
+++ 4245-count-commas-in-range/count-commas-in-range.java
@@ -1,19 +1,5 @@
 class Solution {
     public int countCommas(int n) {
-
-        if (n < 1000) {
-            return 0;
-        }
-        if (n <= 9999) {
-            return n - 1000 + 1;
-        }
-        if (n <= 99999) {
-            return (9999 - 1000 + 1) + (n - 10000 + 1);
-        }
-        if (n <= 999999) {
-            return (99999 - 1000 + 1) + (n - 100000 + 1);
-        }
-        return (999999 - 1000 + 1)
-                + 2 * (n - 1000000 + 1);
+        if(n<1000){return 0;}else{return n-1000+1;}
     }
 }
