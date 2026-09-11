7472caaf872def6706af54aff37a47131d78b4d7
Sameer Varanasi <sameerr.varanasi@gmail.com>
2026-09-11 10:10:13 +0530

    Time: 5 ms (82.80%) | Memory: 46.4 MB (46.93%) - LeetSync

--- 3799-unique-3-digit-even-numbers/unique-3-digit-even-numbers.java
+++ 3799-unique-3-digit-even-numbers/unique-3-digit-even-numbers.java
@@ -1,30 +1,21 @@
 class Solution {
     public int totalNumbers(int[] digits) {
-
         HashSet<Integer> set = new HashSet<>();
-
         for (int i = 0; i < digits.length; i++) {
-
             if (digits[i] == 0) {
                 continue;
             }
-
             for (int j = 0; j < digits.length; j++) {
-
                 if (i == j) {
                     continue;
                 }
-
                 for (int k = 0; k < digits.length; k++) {
-
                     if (i == k || j == k) {
                         continue;
                     }
-
                     if (digits[k] % 2 != 0) {
                         continue;
                     }
-
                     int number = digits[i] * 100
                                + digits[j] * 10
                                + digits[k];
@@ -33,7 +24,6 @@ class Solution {
                 }
             }
         }
-
         return set.size();
     }
 }
